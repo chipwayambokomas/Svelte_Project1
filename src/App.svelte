174fe5528978d86190618c794878ca5cacb2259e
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-05-18 19:03:03 +0200

    Flow control

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,23 @@
   let name = "Saul"
   let title = "My name"
   let titleNew = ""
+  let checked = false;
 </script>
 
 <h1 title={title}>{titleNew}</h1>
-
+{#if checked}
 <div class="div1">
   <Card/>
 <Card title="Different">
   <h1>Hello World</h1>
 </Card>
 </div>
+
+{:else}
+<div>Check the checkbox to show the cards</div>
+{/if}
 <input type="text" bind:value={titleNew}/>
+<input type="checkbox" bind:checked/>
 
 <style>
 
